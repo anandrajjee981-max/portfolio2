389208e3be898ff15353e723ba8e6a0bf88b5956
anand raj <git add README.md>
2026-05-05 11:59:12 +0530

    first commit

---
+++ src/App.jsx
@@ -0,0 +1,15 @@
+import React from 'react'
+// Change this in Heo.jsx
+import "./App.scss";
+import Heo from './pages/Heo';
+
+const App = () => {
+  return (
+   <main>
+<Heo/>
+
+   </main>
+  )
+}
+
+export default App
