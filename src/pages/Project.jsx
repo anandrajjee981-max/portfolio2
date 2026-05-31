e1edc14947abc2edddd80edb28d1826497bf61bb
anand raj <git add README.md>
2026-05-31 16:53:39 +0530

    my resume plus project update

--- src/pages/Project.jsx
+++ src/pages/Project.jsx
@@ -8,6 +8,12 @@ import '../project.scss';
 const Project = () => {
   let files = [
     {
+image : "https://i.pinimg.com/736x/c5/db/f9/c5dbf944b939e5a4332173e63d8f5c06.jpg",
+name : "Affectra",
+livelink : " https://affectra-koea.onrender.com"
+    },
+
+    {
       image : "https://i.pinimg.com/736x/bd/df/ef/bddfef90a6245e481fad5267b9f3fc51.jpg",
       name : "FitNova",
       livelink : "https://summer-internship-hackathon-2026-te.vercel.app/"
